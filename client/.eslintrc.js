75f11d053ed1ec16b147fb7b7f9ca5beaa8501d2
caique.moraes <caique.moraes@compasso.com.br>
2022-09-05 18:08:40 -0300

    envio dos arquivos

---
+++ client/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'standard', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        arrowParens: 'always',
+        printWidth: 80,
+        singleQuote: true,
+        trailingComma: 'all',
+        semi: false,
+        endOfLine: 'lf',
+        tabWidth: 2,
+      },
+    ],
+    'no-unused-vars': 'off',
+    'no-undef': 'off',
+  },
+}
