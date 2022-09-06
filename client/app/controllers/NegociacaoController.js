6080ad41da8d2e3db7afce67715fc4090c89a68b
caique.moraes <caique.moraes@compasso.com.br>
2022-09-06 12:19:55 -0300

    finalização do capítulo 10

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -6,21 +6,18 @@ class NegociacaoController {
     this._inputQuantidade = $('#quantidade')
     this._inputValor = $('#valor')
 
-    this._negociacoes = ProxyFactory.create(
+    this._negociacoes = new Bind(
       new Negociacoes(),
-      ['adiciona', 'esvazia'],
-      (model) => this._negociacoesView.update(model),
+      new NegociacoesView('#negociacoes'),
+      'adiciona',
+      'esvazia',
     )
 
-    this._negociacoesView = new NegociacoesView('#negociacoes')
-    this._negociacoesView.update(this._negociacoes)
-
-    this._mensagem = ProxyFactory.create(new Mensagem(), ['texto'], (model) =>
-      this._mensagemView.update(model),
+    this._mensagem = new Bind(
+      new Mensagem(),
+      new MensagemView('#mensagemView'),
+      'texto',
     )
-
-    this._mensagemView = new MensagemView('#mensagemView')
-    this._mensagemView.update(this._mensagem)
   }
 
   adiciona(event) {
